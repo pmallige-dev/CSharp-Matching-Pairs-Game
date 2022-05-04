dca6c5a09b2953048fb80deb6401ca95a92dd570
pmallige-dev <72284504+pmallige-dev@users.noreply.github.com>
2022-05-04 19:29:49 +0530

    Hide the icon images (By making the fore colour and the back colour as same)

--- MatchingPairsGame/Form1.cs
+++ MatchingPairsGame/Form1.cs
@@ -40,7 +40,7 @@ namespace MatchingPairsGame
                     int randomNumber = random.Next(icons.Count);
                     iconLabel.Text = icons[randomNumber];
 
-                    // iconLabel.ForeColor = iconLabel.BackColor;
+                    iconLabel.ForeColor = iconLabel.BackColor;
                     icons.RemoveAt(randomNumber);
                 }
             }
